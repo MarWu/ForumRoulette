62f3b28833c38beb79ddb04caf88f633b1c20161
MarWu <marcel.wurm@gmx.net>
2018-10-25 09:03:14 +0000

    Simple Tests for Voting

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from django.test import TestCase
+from posts.models import Post
+from django.contrib.auth.models import User
+
+
+class PostVoteTestCase(TestCase):
+    def setUp(self):
+        user1 = User.objects.create(username='TestUser1')
+        user2 = User.objects.create(username='TestUser2')
+        post = Post.objects.create(creator=user1, post_title='TestTitle1', post_text='Test1', pub_date=timezone.now())
+        user1.save()
+        user2.save()
+        post.save()
+
+    def test_no_vote(self):
+        post = Post.objects.get(post_title='TestTitle1')
+        self.assertEqual(post.up_vote_count(), 0)
+
+    def test_vote(self):
+        post = Post.objects.get(post_title='TestTitle1')
+        user1 = User.objects.get(username='TestUser1')
+        post.up_votes_list.add(user1)
+        self.assertEqual(post.up_vote_count(), 1)
