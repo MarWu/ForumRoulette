f056e295cd84d2f4692ac3f785f1a8cdeeaed0b3
Joel <Joel.Flaig@web.de>
2018-11-06 15:30:03 +0100

    Added the detail page Bug can not differ on multiple like elemnts on one page

--- posts/static/js/sidenavigation.js
+++ posts/static/js/sidenavigation.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("sideNavigation").style.width = "250px";
-    document.getElementById("content").style.marginLeft = "250px";
+    document.getElementById("sideNavigation").style.width = "200px";
+    document.getElementById("content").style.marginLeft = "200px";
 }
 
 function closeNav() {
@@ -14,4 +14,12 @@ function like() {
 
 function dislike() {
     document.getElementById("dislike").style.color="red";
+}
+
+function likeComment() {
+    document.getElementById("like_comment").style.color="green";
+}
+
+function dislikeComment() {
+    document.getElementById("dislike_comment").style.color="red";
 }
