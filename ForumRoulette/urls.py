0b44d73f334704f3fa80f68f3b60d4553a31af4d
MarWu <marcel.wurm@gmx.net>
2018-11-24 16:17:10 +0100

    Implemented an image-upload for posts

--- ForumRoulette/urls.py
+++ ForumRoulette/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from ForumRoulette import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('posts.urls')),
     path('user/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
