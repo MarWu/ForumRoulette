60d92e2067883cf32258ff8b42494d25baaafa89
Joel <Joel.Flaig@web.de>
2018-11-25 14:54:29 +0000

    Random Roulette Icon is now responsive. The Form of the add Comment and addPost is now a Textarea.

--- posts/forms.py
+++ posts/forms.py
@@ -3,9 +3,9 @@ from django import forms
 
 class CreatePostForm(forms.Form):
     post_title = forms.CharField(label='Post Title', max_length=200)
-    post_text = forms.CharField(label='Post Text', max_length=500)
+    post_text = forms.CharField(widget=forms.Textarea, label='Post Text', max_length=500)
     post_image = forms.ImageField(label='Post Image (optional)', required=False)
 
 
 class CreateCommentForm(forms.Form):
-    comment_text = forms.CharField(label='Comment Text', max_length=500)
+    comment_text = forms.CharField(widget=forms.Textarea, label='Comment Text', max_length=500)
