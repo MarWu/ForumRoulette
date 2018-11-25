c3a93944d403a88d9d9038fcfc452118ede8a858
Joel <Joel.Flaig@web.de>
2018-11-25 10:42:05 +0000

    added responsive design. fixed close button.

--- posts/static/js/sidenavigation.js
+++ posts/static/js/sidenavigation.js
@@ -1,9 +1,24 @@
 function openNav() {
-    document.getElementById("sideNavigation").style.width = "200px";
-    document.getElementById("content").style.marginLeft = "200px";
+    if (document.documentElement.clientWidth < 750) {
+            document.getElementById("sideNavigation").style.width = "200px";
+            document.getElementById("content").style.display = "none";
+    }
+    else {
+        document.getElementById("sideNavigation").style.width = "200px";
+        document.getElementById("content").style.marginLeft = "200px";
+    }
+
 }
 
 function closeNav() {
+        if (document.documentElement.clientWidth < 750) {
+            document.getElementById("sideNavigation").style.width = "0px";
+            document.getElementById("content").style.display = "flex";
+    }
+    else {
+        document.getElementById("sideNavigation").style.width = "0px";
+        document.getElementById("content").style.marginLeft = "0px";
+    }
     document.getElementById("sideNavigation").style.width = "0";
     document.getElementById("content").style.marginLeft = "0";
 }
